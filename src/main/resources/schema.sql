711e7296dfba8656e229ed5ccd2ec0018724f76e
DiegoVega87 <diegovega@me.com>
2024-02-12 17:34:33 +0100

    Add schema to database

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE person (
+    id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    birthdate VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+
+
+);
