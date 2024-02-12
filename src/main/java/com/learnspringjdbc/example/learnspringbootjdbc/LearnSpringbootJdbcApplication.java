383522667cbef586f9357a29f39e1dcc692a7df2
DiegoVega87 <diegovega@me.com>
2024-02-12 17:31:21 +0100

    First commit

---
+++ src/main/java/com/learnspringjdbc/example/learnspringbootjdbc/LearnSpringbootJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringjdbc.example.learnspringbootjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringbootJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnspringjdbc/example/learnspringbootjdbc/LearnSpringbootJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringjdbc.example.learnspringbootjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
