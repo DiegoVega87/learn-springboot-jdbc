fb2fceb5a24591363df4e08083de9635fdfebbdd
DiegoVega87 <diegovega@me.com>
2024-02-12 19:43:17 +0100

    Add Repository class

---
+++ src/main/java/com/learnspringjdbc/example/jdbc/PersonJdbcRepository.java
@@ -0,0 +1,45 @@
+package com.learnspringjdbc.example.jdbc;
+
+import com.learnspringjdbc.example.entities.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonJdbcRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate; // JdbcTemplate is a class provided by Spring to make it easier to work with JDBC
+
+    // Add the preformatted SQL queries as constants
+    private static final String INSERT_QUERY = """
+            INSERT INTO person (id, name, birth_date, address, city, state)
+        VALUES(?, ?, ?, ?,?,?)
+        """;
+    private static final String DELETE_QUERY = """
+            DELETE FROM person WHERE id = ?
+        """;
+
+    private static final String SELECT_QUERY = """
+            SELECT *
+            FROM person
+            Where id = ? """;
+
+    // Add the methods to interact with the database
+    public void insert(Person person){
+        jdbcTemplate.update(INSERT_QUERY, person.getId(), person.getName(), person.getBirthDate(),
+                person.getAddress(), person.getCity(), person.getState());
+    }
+
+    public void deleteById(int id){
+        jdbcTemplate.update(DELETE_QUERY, id);
+    }
+
+    public Person findById(int id){
+        //ResultSet -> Bean -> RowMapper
+        return jdbcTemplate.queryForObject(SELECT_QUERY,
+                new BeanPropertyRowMapper<Person>(Person.class), id);
+    }
+
+}
